2eb0967474aded194d8130b01f5c41949d9a62dd
Luz <luz.d.saucedo@kp.org>
2026-02-21 11:57:14 -0600

    parametro dinamico

--- app/detalle.js
+++ app/detalle.js
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import { Stack } from 'expo-router';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function Detalle() {
+  const { title, id } = useLocalSearchParams();
   return (
     <View>
-      <Text>Vista detalle</Text>
+      <Text>Vista detalle de {title} con ID {id}</Text>
     </View>
   );
 }
--- app/index.js
+++ app/index.js
@@ -4,30 +4,38 @@ import { useRouter } from 'expo-router';
 
 
 export default function App() {
-  const ruta = useRouter();
-  const listaRutas = [
-    { id: 1, title: "React Native" },
-    { id: 2, title: "Expo Router" },
-    { id: 3, title: "JavaScript" },
-    { id: 4, title: "Arquitectura" },
-    { id: 5, title: "Proyecto Final" },
-  ];
-
-  return (
-
-    <View style={styles.container}>
-     <Pressable onPress={() => ruta.push('/detalle')}>
-      <Text>Ir a la vista detalle</Text>
-     </Pressable>
-    </View>
-  );
+    const ruta = useRouter();
+    const listaRutas = [
+        { id: 1, title: "React Native" },
+        { id: 2, title: "Expo Router" },
+        { id: 3, title: "JavaScript" },
+        { id: 4, title: "Arquitectura" },
+        { id: 5, title: "Proyecto Final" },
+    ];
+    
+    return (
+        
+        <View style={styles.container}>
+        {
+            listaRutas.map((rutaItem) => (
+                <Pressable onPress={() => ruta.push({
+                    pathname:'/detalle',
+                    params: { title: rutaItem.title, id: rutaItem.id}
+                })}>
+                <Text>Ir a la vista detalle de {rutaItem.title}</Text>
+                </Pressable>
+            ))
+        }
+        
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
