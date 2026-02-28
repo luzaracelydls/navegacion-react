b82d6308b7fb008546a9e47dcb8e9fe5854e1fc9
Luz <luz.d.saucedo@kp.org>
2026-02-28 12:02:02 -0600

    iconos

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,12 +1,21 @@
-import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Tabs } from 'expo-router';
 
-export default function Layout(){
+export default function TabLayout() {
     return(
-        <Tabs screenOptions={ {tabBarActiveTintColor: '#453823'}}
-        >
-            <Tabs.Screen name="perfil"></Tabs.Screen>
-            <Tabs.Screen name="buscar"></Tabs.Screen>
-            <Tabs.Screen name="config"></Tabs.Screen>
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+
+            <Tabs.Screen name="index" options={{ titulo: 'Inicio' }} />
+            <Tabs.Screen name="buscar" options={{ titulo: 'Buscar', 
+                tabBarIcon: ({color}) => <AntDesign name="search1" size={24} color={color} />
+             }} />
+            <Tabs.Screen name="perfil" options={{ 
+                titulo: 'Perfil', 
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+                params: {mensaje: 'holaMundo'}
+            }}
+            />   
         </Tabs>
     )
 }
