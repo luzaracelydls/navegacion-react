04f67bfcceb5f79deb32551685fd05bdfddba223
Luz <luz.d.saucedo@kp.org>
2026-02-28 11:26:32 -0600

    cambios

--- app/(tabs)/config.js
+++ app/(tabs)/config.js
@@ -1,9 +1,5 @@
-import { View, Text } from "react-native";
+
 
 export default function Config() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Config</Text>
-    </View>
-  );
+ 
 }
